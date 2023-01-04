ae32d695f5980a8b80f1d09566b92236f6d107fb
SerhiiHavryliuk <havryliuk.serhii@gmail.com>
2023-01-04 18:32:52 +0200

    add emploees from files

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import './style.css'
 import ListItem from "../ListItem";
+import {dataEmp} from '../../dataEmploees'
 
 class List extends React.Component {
     render() {
-        const dataEmp = [
-            {id: 1, name: 'Serhii', salary:'5100', isBonus: true },
-            {id: 1, name: 'Andry', salary:'3400', isBonus: false },
-            {id: 1, name: 'Pavel', salary:'3500', isBonus: true },
-            {id: 1, name: 'Teti', salary:'5000', isBonus: true },
-            {id: 1, name: 'Makar', salary:'5000', isBonus: true },
-        ]
         return (
             <div data= {dataEmp} className='listWrap'>
                 {dataEmp.map(item => (
